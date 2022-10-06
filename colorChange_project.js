48a2e0221479d5117b14a6acd208dc65be899f55
oluwatunmise ikusika <oluwatunmisemary2506@gmail.com>
2022-10-06 15:36:42 +0100

    Added changes

--- colorChange_project.js
+++ colorChange_project.js
@@ -1,6 +1,9 @@
 function change_bgColor() 
 {
-
-  document.getElementById("inner_div").style.backgroundColor = "orange"
-  document.getElementById("mytext").innerText = "ORANGE"
+  //background colour collection
+  let Colours = ["#000000" , "#A52A2A" , "#0000FF" ,"008000"]
+  //To get each colour from the collection 
+  let index = Math.floor(Math.random()*Colours.length)
+  let ColourToUse = Colours[index]
+  document.getElementById("outer_div").style.backgroundColor = ColourToUse
 }

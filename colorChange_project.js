b64ec248421ec8d52acb6362dabc5fc95717f8b0
oluwatunmise ikusika <oluwatunmisemary2506@gmail.com>
2022-10-07 08:32:16 +0100

    Added new features

--- colorChange_project.js
+++ colorChange_project.js
@@ -1,9 +1,19 @@
 function change_bgColor() 
 {
   //background colour collection
-  let Colours = ["#000000" , "#A52A2A" , "#0000FF" ,"008000"]
+  let Colours = ["#000000" , "#A52A2A" , "#0000FF" , "#A52A2A" ,"008000" , "#FFD700" , "#808080"]
   //To get each colour from the collection 
   let index = Math.floor(Math.random()*Colours.length)
   let ColourToUse = Colours[index]
   document.getElementById("outer_div").style.backgroundColor = ColourToUse
 }
+
+let myTimer = setInterval(hide_btn , 5000)
+
+function hide_btn()
+{
+ document.getElementById("changer_btn").style.visibility = "visible" 
+}
+
+window.alert("A button will appear after 5 secs")
+

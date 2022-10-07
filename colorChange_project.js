8a81eae49a206a2ba5bf39dbc1b666857cf26f5a
oluwatunmise ikusika <oluwatunmisemary2506@gmail.com>
2022-10-07 11:48:01 +0100

    cleanup code

--- colorChange_project.js
+++ colorChange_project.js
@@ -1,7 +1,9 @@
+
+//background colour collection
+let colours = ["#000000", "#A52A2A", "#0000FF","#A52A2A", "008000", "#FFD700", "#808080"]
+
 function change_bgColor() 
 {
-  //background colour collection
-  let colours = ["#000000", "#A52A2A", "#0000FF","#A52A2A", "008000", "#FFD700", "#808080"]
   //To get each colour from the collection 
   let index = Math.floor(Math.random()*colours.length)
   let colourToUse = colours[index]
@@ -12,8 +14,7 @@ let myTimer = setInterval(display_btn , 5000)
 
 function display_btn()
 {
- document.getElementById("changer_btn").style.visibility = "visible" 
+  document.getElementById("changer_btn").style.visibility = "visible" 
 }
 
-window.alert("A button will appear after 5 secs")
 

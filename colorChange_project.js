20135c51401c93ad87ee511ce63420b53e72b0fe
oluwatunmise ikusika <oluwatunmisemary2506@gmail.com>
2022-10-10 19:37:16 +0100

    Added new changes

--- colorChange_project.js
+++ colorChange_project.js
@@ -18,9 +18,26 @@ function hide_btn()
 window.alert("A button will appear after 5 secs")
 
 
-let i = 2
-while (i<10)
-  {
-   console.log(i);i++
-  }
+let i = 2 
+//while(i<10)
+  //{
+ // console.log(i);
+  // i++;
+ // }
+ 
 
+
+
+function getEvenNumbers(first_number, second_number) 
+{
+  while (first_number < second_number)
+ {
+  if (first_number % 2 == 0) {
+   console.log(first_number);
+  }
+  first_number++;
+ }  
+ 
+}
+ 
+getEvenNumbers(15, 50)
